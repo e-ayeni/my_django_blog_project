7be00ef54335c62b5db8fcd2ed44a439ac3cb6f7
e-ayeni <ade.eayeni@gmail.com>
2019-07-31 04:18:35 +0100

    UserCreationForm created for functional purposes only. Detailed registration form for database coming next

--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # from django import forms
 
 
 def register(request):
     if request.method == 'POST':
+        # if the request method (from the template) is post, then create the User
+
         form = UserCreationForm(request.POST)
-        if form.is_valid()
+        if form.is_valid():
+            form.save()
+            return redirect('/thanks/')
+    else:
+        form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
